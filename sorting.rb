2927d318a061408dcc8c8fbd90241287810c1562
Jeffrey Gisin <jgisin@gmail.com>
2016-01-14 11:15:53 -0700

    merge sort in progress

--- sorting.rb
+++ sorting.rb
@@ -37,31 +37,61 @@ def insertion_sort(array)
 end
 
 def bubble_sort(array)
-  num_swaps = 0
+  num_swaps = true
 
-  loop do
+  while num_swaps
+    num_swaps = false
     array.each_with_index do |num, idx|
       if !array[idx+1].nil? && num > array[idx+1]
         swap(array, idx, idx+1)
-        num_swaps += 1
+        num_swaps = true
       end
     end
-    break if num_swaps == 0
-    num_swaps = 0
   end
   array
 end
 
 def merge(sub_array1,sub_array2)
-  # 
+  # pop off elements from sub_arrays until they are length 0
+    # Compare sub_array1[0] with sub_array2[0]
+    # if sub_array1[0] is smaller add to new_array, otherwise use other
+  new_array = []
 
+  until sub_array1.length == 0 || sub_array2.length == 0
+    #binding.pry
+    if sub_array1[0] < sub_array2[0]
+      new_array << sub_array1.shift
+    else
+      new_array << sub_array2.shift
+    end
+  end
+
+  unless sub_array1
+    new_array + sub_array1
+  else
+    new_array + sub_array2
+  end
+  new_array
 end
 
 def merge_sort(array)
-  new_array = []
 
+  return array if array.length ==  1 
+  half = array.length/2
+  if array.length > 2
+    array_left = array[0..half]
+    array_right = array[half + 1..array.length - 1]
+  else
+    array_left = [array[0]]
+    array_right = [array[1]]
+  end
+
+  new_array =  merge(merge_sort(array_left),merge_sort(array_right))
+
+ 
 # Split array in half until sub-array length is 1
-# Put into groups of 2 that are sorted
+# merge into groups that are sorted
 
-  new_array
 end
+
+puts merge_sort([1,3,7,2,5,8,9,2,3,4])
